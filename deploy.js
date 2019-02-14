795e8f940d6c431b66673ebaee1c94b46fbc3dbf
Owner <minemidnightmc@gmail.com>
2019-02-13 21:03:06 -0500

    Small fixes/changes

--- deploy.js
+++ deploy.js
@@ -20,77 +20,80 @@ const imageTag = {
 	development: "latest"
 }[process.env.NODE_ENV];
 
-async function execCommand(command) {
-	console.log(`$ ${command}`);
-	const out = await await new Promise((resolve, reject) => {
-		exec(command, (err, stdout, stderr) => {
-			if(err) reject(err);
-			else if(stderr) reject(stderr);
-			else if(stdout) resolve(stdout);
+const actions = {
+	async execCommand(command) {
+		console.log(`$ ${command}`);
+		const out = await await new Promise((resolve, reject) => {
+			exec(command, (err, stdout, stderr) => {
+				if(err) reject(err);
+				else if(stderr) reject(stderr);
+				else if(stdout) resolve(stdout);
+			});
 		});
-	});
 
-	console.log(`${out}\n`);
-	return out;
-}
+		console.log(`${out}\n`);
+		return out;
+	},
+	async deleteNamespace() {
+		try {
+			await actions.execCommand(`kubectl delete namespaces ${namespace}`);
+		} catch(err) { } // eslint-disable-line no-empty
+	},
+	async createNamespace() {
+		await actions.execCommand(`kubectl create namespace ${namespace}`);
+	},
+	async applyConfig() {
+		for(const [name, values] of Object.entries(config)) {
+			await actions.execCommand(`kubectl create configmap ${name} ${
+				Object.entries(values).map(([key, value]) => `--from-literal=${key}=${value}`).join(" ")
+			} --namespace=${namespace}`);
+		}
+	},
+	async applySecrets() {
+		for(const [name, values] of Object.entries(secret)) {
+			await actions.execCommand(`kubectl create secret generic ${name} ${
+				Object.entries(values).map(([key, value]) => `--from-literal=${key}=${value}`).join(" ")
+			} --namespace=${namespace}`);
+		}
+	},
+	async applyDirectory(directory) {
+		const items = await fs.readdir(directory);
+
+		for(const item of items) {
+			const resolvedPath = path.resolve(directory, item);
+
+			const stat = await fs.stat(resolvedPath);
+			if(stat.isDirectory()) await actions.applyDirectory(resolvedPath);
+			else if(stat.isFile()) await actions.applyFile(resolvedPath);
+		}
+	},
+	async applyFile(filePath) {
+		const file = await fs.readFile(filePath, "utf8");
+
+		const configured = file.replace(/\{\{namespace\}\}/g, namespace)
+			.replace(/\{\{tag\}\}/g, imageTag)
+			.replace(/\{\{hostname\}\}/g, hostname);
+
+		const name = `${(Date.now() + process.hrtime().reduce((a, b) => a + b)).toString(36)}.yml`;
+		const location = path.resolve(__dirname, name);
+
+		await fs.writeFile(location, configured);
+		await actions.execCommand(`kubectl apply -f ${location}`);
+		await fs.unlink(location);
+
+		return location;
+	}
+};
 
 async function init() {
-	try {
-		await execCommand(`kubectl delete namespaces ${namespace}`);
-	} catch(err) {} // eslint-disable-line no-empty
+	await actions.deleteNamespace();
+	await actions.createNamespace();
+	await actions.applyConfig();
+	await actions.applySecrets();
 
-	await execCommand(`kubectl create namespace ${namespace}`);
-
-	for(const [name, values] of Object.entries(config)) {
-		await execCommand(`kubectl create configmap ${name} ${
-			Object.entries(values).map(([key, value]) => `--from-literal=${key}=${value}`).join(" ")
-		} --namespace=${namespace}`);
-	}
-
-	for(const [name, values] of Object.entries(secret)) {
-		await execCommand(`kubectl create secret generic ${name} ${
-			Object.entries(values).map(([key, value]) => `--from-literal=${key}=${value}`).join(" ")
-		} --namespace=${namespace}`);
-	}
-
-	await fs.mkdir(path.resolve(__dirname, "configured"));
-	await applyDirectory(path.resolve(__dirname, "services"));
-	await applyDirectory(path.resolve(__dirname, "other"));
-	await applyDirectory(path.resolve(__dirname, "deployments"));
-
-	const configuredFiles = await fs.readdir(path.resolve(__dirname, "configured"));
-	for(const file of configuredFiles) await fs.unlink(path.resolve(__dirname, "configured", file));
-	await fs.rmdir(path.resolve(__dirname, "configured"));
-}
-
-async function applyDirectory(directory) {
-	const items = await fs.readdir(directory);
-
-	for(const item of items) {
-		const resolvedPath = path.resolve(directory, item);
-
-		const stat = await fs.stat(resolvedPath);
-		if(stat.isDirectory()) {
-			await applyDirectory(resolvedPath);
-		} else if(stat.isFile()) {
-			const configuredFile = await configureFile(resolvedPath);
-			await execCommand(`kubectl apply -f ${configuredFile}`);
-		}
-	}
-}
-
-async function configureFile(filePath) {
-	const file = await fs.readFile(filePath, "utf8");
-
-	const configured = file.replace(/\{\{namespace\}\}/g, namespace)
-		.replace(/\{\{tag\}\}/g, imageTag)
-		.replace(/\{\{hostname\}\}/g, hostname);
-
-	const name = `${(Date.now() + process.hrtime().reduce((a, b) => a + b)).toString(36)}.yml`;
-	const location = path.resolve(__dirname, "configured", name);
-	await fs.writeFile(location, configured);
-
-	return location;
+	await actions.applyDirectory(path.resolve(__dirname, "services"));
+	await actions.applyDirectory(path.resolve(__dirname, "other"));
+	await actions.applyDirectory(path.resolve(__dirname, "deployments"));
 }
 
 init();
